7b5758ec78f96cce67b94ea87fa869a95ea8fa54
LautaroLinari <lautarolinari18@hotmail.com>
2023-12-04 19:16:28 -0300

    2do commit

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -54,7 +54,7 @@ const UserForm = () => {
 
             const resumen = document.querySelector("#resume")
             //Precio M2
-            const precioM2 = userData.m2 * 40.55;
+            const precioM2 = userData.m2 * 40.50;
             //Sacar edad
             const edad = 2024 - userData.anio;
             //Precio dependiendo la edad del lugar
@@ -64,17 +64,17 @@ const UserForm = () => {
 
             //Valor total dependiend la edad y teniendo en cuenta los factores de tipo y ubicacion
             if (edad >= 200) {
-                let precioTotal1 = precioM2 + precioEdad1 * userData.tipo * userData.ubi;
+                let precioTotal1 =  precioEdad1 * userData.tipo * userData.ubi;
                 precioTotal1 = parseFloat(Math.round(precioTotal1 * 100) / 100).toFixed(2);
                 resumen.innerText = precioTotal1;
 
             } if (edad >= 100 && edad < 200) {
-                let precioTotal2 = precioM2 + precioEdad2 * userData.tipo * userData.ubi;
+                let precioTotal2 =  precioEdad2 * userData.tipo * userData.ubi;
                 precioTotal2 = parseFloat(Math.round(precioTotal2 * 100) / 100).toFixed(2);
                 resumen.innerText = precioTotal2;
 
             } if (edad < 100) {
-                let precioTotal3 = precioM2 + precioTEdad3 * userData.tipo * userData.ubi;
+                let precioTotal3 =  precioTEdad3 * userData.tipo * userData.ubi;
                 precioTotal3 = parseFloat(Math.round(precioTotal3 * 100) / 100).toFixed(2);
                 resumen.innerText = precioTotal3;
             }
@@ -89,15 +89,15 @@ const UserForm = () => {
                 <div className="p-2">
                     <label htmlFor="" className="flex font-semibold text-xl p-2">Tipo de Propiedad</label>
                     <select name="" id="tipoPropiedad" onChange={handleTipoPropiedad} className="text-start text-xl w-full" required>
-                        <option value="">-- Seleccione Tipo Propiedad --</option>
+                        <option value="">-- Seleccionar Propiedad --</option>
                         {displayData}
                     </select>
                 </div>
 
                 <div className="p-2">
-                    <label htmlFor="" className="flex font-semibold text-xl p-2">Ubicacion</label>
+                    <label htmlFor="" className="flex font-semibold text-xl p-2">Ubicación</label>
                     <select name="" id="ubicacion" onChange={handleUbicacion} className="text-start text-xl w-full" required>
-                    <option value="">-- Seleccione Ubicacion --</option>
+                    <option value="">-- Seleccionar Ubicación --</option>
                         {displayData2}
                     </select>
                 </div>
@@ -108,21 +108,20 @@ const UserForm = () => {
                 </div>
 
                 <div className="p-2">
-                    <label htmlFor="" className="flex font-semibold text-xl p-2">Ingresar Año de Construccion:</label>
+                    <label htmlFor="" className="flex font-semibold text-xl p-2">Ingresar Año de Construcción:</label>
                     <input id="año" className="w-full text-xl" type="number" value={userData.anio} onChange={handleAnioChange} min={1800} max={2024} required/>
                 </div>
 
-                <div className="flex flex-col items-center justify-center p-8 w-full">
+                <div className="flex flex-col items-center justify-center p-6 w-full">
                     <div className="flex items-center justify-center">
-                        <button id="btnForm" type="submit" className="text-center text-xl mt-4 p-2 bg-slate-300 hover:bg-green-500 text-black hover:text-white rounded-xl">Cotizar</button>
+                        <button id="btnForm" type="submit" className="text-center text-xl mt-4 p-2 bg-slate-300 hover:bg-green-700 text-black hover:text-white rounded-xl">Cotización</button>
                     </div>
                 </div>
 
-                <div className="flex flex-row items-center justify-center p-8 w-full">
-                    <p className='text-black text-2xl font-semibold text-center p-2'>Precio Estimado: $</p>
-                    <p id='resume' className='text-black text-2xl font-bold text-center'> 0,00 </p> 
+                <div className="flex flex-row items-center justify-center p-6 w-full">
+                    <p className='text-green-700 text-2xl font-semibold text-center'>Precio Estimado: $</p>
+                    <p id='resume' className='text-green-700 text-2xl font-bold text-center'> 0,00 </p> 
                 </div>
-
 
             </div>
         </form>
@@ -130,3 +129,8 @@ const UserForm = () => {
   }
   
   export default UserForm  
+
+  /*Hola Pablo, disculpa te molesto ya que me corregiste el trabajo pasado jajaja, queria consultar un par de cosas del proyecto final: la primera es que yo hice todo lo relacionado 
+  al formulario en un solo componente (la duda es si deberia separarlo en varios), despues tambien 
+
+  */
