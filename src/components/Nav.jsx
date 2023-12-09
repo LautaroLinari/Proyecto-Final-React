568dfc714a0268841932c0661479ba22c61a454d
LautaroLinari <lautarolinari18@hotmail.com>
2023-12-09 19:04:02 -0300

    bug imgs

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -31,7 +31,7 @@ import { useEffect, useState } from "react"
             <label className="relative inline-flex items-center">
                 <input type="checkbox" value="" className="sr-only peer" />
                 <div onClick={handleChangeTheme} className="cursor-pointer w-11 h-6 border rounded border-gray-200 bg-gray-200 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-neutral-500"></div>
-                <img src="./assets/icons/moon.png" alt="" onClick={handleChangeTheme} className="ml-5 h-6 cursor-pointer"/>
+                <img src="../src/assets/icons/moon.png" alt="moon" onClick={handleChangeTheme} className="ml-5 h-6 cursor-pointer"/>
             </label>
     
         </div>
