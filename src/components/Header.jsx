9b612387ccf7f0f1e26befaa64317342998c0c88
LautaroLinari <lautarolinari18@hotmail.com>
2023-12-09 18:48:28 -0300

    icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <>
-      <div className="bg-[url('../src/assets/img/bg-header.jpg')] bg-no-repeat bg-center bg-cover flex items-left justify-start p-44 w-full">
+      <div className="bg-[url('./assets/img/bg-header2.jpg')] bg-no-repeat bg-center bg-cover flex items-left justify-start p-44 w-full">
           <h1 className="font-bold text-xl md:text-3xl animate-pulse text-white p-4 rounded-full">Cotizador de Seguros para Casas, <br/> Depto, Duplex, Oficinas, etc</h1>
       </div>
     </>
