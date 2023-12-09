57e38937a986945588c244ba7a438933c034487d
LautaroLinari <lautarolinari18@hotmail.com>
2023-12-09 19:11:23 -0300

    icons

--- src/components/Cobertura.jsx
+++ src/components/Cobertura.jsx
@@ -8,9 +8,9 @@ function Cobertura() {
 
             <div className=" p-8 border rounded border-white bg-white dark:bg-neutral-800 text-black dark:text-white shadow-2xl hover:border-neutral-600">
                 <DivCobertura title='Plan Básico:'/>                         
-                <DivCobertura image="../src/assets/icons/house-fire.png" text="Contra incendios:" valor="$750"/>
-                <DivCobertura image="../src/assets/icons/stolen-house.png" text="Robos en su hogar:" valor="$750"/>
-                <DivCobertura image="../src/assets/icons/damaged-house.png" text="Daños en su hogar:" valor="$500"/>
+                <DivCobertura image="bg-[url('../assets/icons/house-fire.png')]" text="Contra incendios:" valor="$750"/>
+                <DivCobertura image="bg-[url('../assets/icons/stolen-house.png')]" text="Robos en su hogar:" valor="$750"/>
+                <DivCobertura image="bg-[url('../assets/icons/damaged-house.png')]" text="Daños en su hogar:" valor="$500"/>
                 <div className="pt-6">
                     <h2 className="text-xl md:text-2xl text-white font-bold p-2 bg-green-700 rounded"> Precio Total: $2000</h2>
                 </div>
@@ -19,12 +19,12 @@ function Cobertura() {
 
             <div className=" mt-10 border border-white rounded p-8 bg-white dark:bg-neutral-800 dark:text-white shadow-2xl hover:border-neutral-600">
                 <DivCobertura title='Plan Completo:'/>   
-                <DivCobertura image="../src/assets/icons/house-fire.png" text="Contra incendios:" valor="$750"/>
-                <DivCobertura image="../src/assets/icons/stolen-house.png" text="Robos en su hogar:" valor="$750"/>
-                <DivCobertura image="../src/assets/icons/damaged-house.png" text="Daños en su hogar:" valor="$500"/>
-                <DivCobertura image="../src/assets/icons/appliance.png" text="Reposición Electrodomesticos:" valor="$750"/>
-                <DivCobertura image="../src/assets/icons/civil.png" text="Responsabilidad Civil:" valor="$400"/>
-                <DivCobertura image="../src/assets/icons/glasses.png" text="Cristales:" valor="$350"/>
+                <DivCobertura image="bg-[url('../assets/icons/house-fire.png')]" text="Contra incendios:" valor="$750"/>
+                <DivCobertura image="bg-[url('../assets/icons/stolen-house.png')]" text="Robos en su hogar:" valor="$750"/>
+                <DivCobertura image="bg-[url('../assets/icons/damaged-house.png')]" text="Daños en su hogar:" valor="$500"/>
+                <DivCobertura image="bg-[url('../assets/icons/appliance.png')]" text="Reposición Electrodomesticos:" valor="$750"/>
+                <DivCobertura image="bg-[url('../assets/icons/civil.png')]" text="Responsabilidad Civil:" valor="$400"/>
+                <DivCobertura image="bg-[url('../assets/icons/glasses.png')]" text="Cristales:" valor="$350"/>
                 <div className="pt-6">
                     <h2 className="text-xl md:text-2xl text-white font-bold p-2 bg-green-700 rounded pointer"> Precio Total: $3500</h2>
                 </div>
